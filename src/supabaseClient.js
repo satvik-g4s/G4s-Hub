9a25b2e90bb3f43ed350dc93652e82859ebd71c4
satvik-g4s <7weekg4s@gmail.com>
2026-04-30 09:37:50 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://fzlfedubjblnhrivxvlw.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6bGZlZHViamJsbmhyaXZ4dmx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4NTM1NjcsImV4cCI6MjA5MDQyOTU2N30.mkVYLBCW2Y56OsBKOFiQGKaz_27JXCzS9Wx-JZ9YPKQ'
+const supabaseUrl = process.env.supabaseUrl
+const supabaseKey = process.env.supabaseKey
 
 export const supabase = createClient(supabaseUrl, supabaseKey)

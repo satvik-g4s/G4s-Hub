bba1a39b2c32cecdb0960aec2ed885637e41bca8
satvik-g4s <7weekg4s@gmail.com>
2026-04-30 10:01:00 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.supabaseUrl
-const supabaseKey = process.env.supabaseKey
+const supabaseUrl = VITE_SUPABASE_URL
+const supabaseKey = VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
